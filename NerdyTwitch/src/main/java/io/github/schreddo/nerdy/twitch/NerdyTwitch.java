0d0c840944ef2c21c95b7ad5311ab018f89ae354
Sebastian <seb-lue@t-online.de>
2018-06-10 13:54:25 +0200

    Add new functionalities and reconstruction project strucktor
    - reconstruction project strucktor
    - add handling IRC connection
    - add join channel
    - add leave channel
    - add read message
    - add send message
    - add send whisper message

--- NerdyTwitch/src/main/java/io/github/schreddo/nerdy/twitch/NerdyTwitch.java
+++ NerdyTwitch/src/main/java/io/github/schreddo/nerdy/twitch/NerdyTwitch.java
@@ -19,11 +19,119 @@
 //
 // Created By: SCHREDDO[Sebastian]
 // Created On: 21.05.2018
-// Last Edited On: 21.05.2018
+// Last Edited On: 10.06.2018
 // Language: Java
 //
 package io.github.schreddo.nerdy.twitch;
 
+import io.github.schreddo.nerdy.twitch.irc.TwitchIRCClient;
+import io.github.schreddo.nerdy.twitch.irc.connection.TwitchIRCConnection;
+
 public class NerdyTwitch {
 
+	private TwitchIRCConnection connection;
+	private boolean extendedInformation;
+	
+	public TwitchIRCConnection getConnection() {
+		return connection;
+	}
+	public void setConnection(TwitchIRCConnection connection) {
+		this.connection = connection;
+	}
+	public boolean isExtendedInformation() {
+		return extendedInformation;
+	}
+	public void setExtendedInformation(boolean extendedInformation) {
+		this.extendedInformation = extendedInformation;
+	}
+	
+	public NerdyTwitch() {
+		setConnection(new TwitchIRCConnection());
+		setExtendedInformation(true);
+	}
+	
+	public NerdyTwitch(String username, String userPassword) {
+		setConnection(new TwitchIRCConnection(username, userPassword));
+		setExtendedInformation(true);
+	}
+	
+	public NerdyTwitch(String username, String userPassword, boolean useSSL) {
+		setConnection(new TwitchIRCConnection(username, userPassword, useSSL));
+		setExtendedInformation(true);
+	}
+	
+	public NerdyTwitch(String username, String userPassword, String serverAddress, boolean useSSL) {
+		this(username, userPassword, serverAddress, useSSL, true);
+	}
+	
+	public NerdyTwitch(String username, String userPassword, String serverAddress, int serverPort) {
+		this(username, userPassword, serverAddress, serverPort, true);
+	}
+	
+	public NerdyTwitch(String username, String userPassword, String serverAddress, boolean useSSL, boolean extendedInformation) {
+		setConnection(new TwitchIRCConnection(username, userPassword, serverAddress, useSSL));
+		setExtendedInformation(extendedInformation);
+	}
+	
+	public NerdyTwitch(String username, String userPassword, String serverAddress, int serverPort, boolean extendedInformation) {
+		setConnection(new TwitchIRCConnection(username, userPassword, serverAddress, serverPort));
+		setExtendedInformation(extendedInformation);
+	}
+	
+	public void setConnectionInformation(String username, String userPassword, boolean useSSL, boolean extendedInformation) {
+		TwitchIRCClient.disconnect(getConnection());
+		
+		setConnection(new TwitchIRCConnection(username, userPassword, useSSL));
+		setExtendedInformation(true);
+	}
+	
+	public void setConnectionInformation(String username, String userPassword, String serverAddress, boolean useSSL, boolean extendedInformation) {
+		TwitchIRCClient.disconnect(getConnection());
+		
+		setConnection(new TwitchIRCConnection(username, userPassword, serverAddress, useSSL));
+		setExtendedInformation(extendedInformation);
+	}
+	
+	public void setConnectionInformation(String username, String userPassword, String serverAddress, int serverPort, boolean extendedInformation) {
+		TwitchIRCClient.disconnect(getConnection());
+		
+		setConnection(new TwitchIRCConnection(username, userPassword, serverAddress, serverPort));
+		setExtendedInformation(extendedInformation);
+	}
+	
+	public int connect() {
+		return TwitchIRCClient.connect(getConnection(), isExtendedInformation());
+	}
+	
+	public int disconnect() {
+		return TwitchIRCClient.disconnect(getConnection());
+	}
+	
+	public int restart() {
+		return TwitchIRCClient.restart(getConnection(), isExtendedInformation());
+	}
+	
+	public int joinChannel(String channelName) {
+		return TwitchIRCClient.joinChannel(channelName, getConnection());
+	}
+	
+	public int leaveChannel(String channelName) {
+		return TwitchIRCClient.departChannel(channelName, getConnection());
+	}
+	
+	public String readMessage() {
+		return TwitchIRCClient.readMessage(getConnection());
+	}
+	
+	public int sendServerMessage(String message) {
+		return TwitchIRCClient.sendServerMessage(message, getConnection());
+	}
+
+	public int sendChannelMessage(String channelName, String message) {
+		return TwitchIRCClient.sendChannelMessage(channelName, message, getConnection());
+	}
+	
+	public int sendWisperMessage(String username, String message) {
+		return TwitchIRCClient.sendWisperMessage(username, message, getConnection());
+	}
 }
--- NerdyTwitch/src/main/java/io/github/schreddo/nerdy/twitch/irc/TwitchIRCClient.java
+++ NerdyTwitch/src/main/java/io/github/schreddo/nerdy/twitch/irc/TwitchIRCClient.java
@@ -19,97 +19,32 @@
 //
 // Created By: SCHREDDO[Sebastian]
 // Created On: 21.05.2018
-// Last Edited On: 21.05.2018
+// Last Edited On: 10.06.2018
 // Language: Java
 //
 package io.github.schreddo.nerdy.twitch.irc;
 
-import io.github.schreddo.nerdy.twitch.irc.abstracts.IRCClient;
-import io.github.schreddo.nerdy.twitch.irc.connection.IRCConnection;
+import io.github.schreddo.nerdy.twitch.irc.connection.TwitchIRCConnection;;
 
-public class TwitchIRCClient extends IRCClient{
+public class TwitchIRCClient {
 	
-	private boolean extendedInformation;
-	
-	public boolean getExtendedInformation() {
-		return extendedInformation;
-	}
-	public void setExtendedInformation(boolean extendedInformation) {
-		this.extendedInformation = extendedInformation;
-	}
-
-	public TwitchIRCClient() {
-		this("", "", "irc.twitch.tv", 443, false);
+	public static int connect(TwitchIRCConnection connection) {
+		return connect(connection, true);
 	}
 	
-	public TwitchIRCClient(String botUsername, String botPassword) {
-		this(botUsername, botPassword, "irc.twitch.tv", 443, false);
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, boolean useSSL) {
-		this(botUsername, botPassword, "irc.twitch.tv", 443, useSSL);
+	public static int connect(TwitchIRCConnection connection, boolean extendedInformation) {
 		
-		if (!useSSL) {
-			setServerPort(6667);
-		}
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, int serverPort) {
-		this(botUsername, botPassword, "irc.twitch.tv", 443, false);
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, boolean useSSL, boolean extendedInformation) {
-		this(botUsername, botPassword, "irc.twitch.tv", useSSL, extendedInformation);
-		
-		if (!useSSL) {
-			setServerPort(6667);
-		}
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, String serverAddress, boolean useSSL) {
-		this(botUsername, botPassword, serverAddress, 443, useSSL);
-		
-		if (!useSSL) {
-			setServerPort(6667);
-		}
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, String serverAddress, int serverPort) {
-		this(botUsername, botPassword, serverAddress, serverPort, false);
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, int serverPort, boolean extendedInformation) {
-		this(botUsername, botPassword, "irc.twitch.tv", 443, extendedInformation);
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, String serverAddress, boolean useSSL, boolean extendedInformation) {
-		this(botUsername, botPassword, serverAddress, 443, extendedInformation);
-		
-		if (!useSSL) {
-			setServerPort(6667);
-		}
-	}
-	
-	public TwitchIRCClient(String botUsername, String botPassword, String serverAddress, int serverPort, boolean extendedInformation) {
-		super(serverAddress, serverPort, botUsername, botPassword);
-		
-		setExtendedInformation(extendedInformation);
-	}
-	
-	@Override
-	public int connect() {
-		
-		if (IRCConnection.getConnectionStatus() == 0) {
-			int isConnected = IRCConnection.connect(getServerAddress(), getServerPort());
+		if (connection.getConnectionStatus() == 0) {
+			int isConnected = connection.connect();
 			
 			if (isConnected == 0) {
-				IRCConnection.send("PASS " + getBotPassword() + "\r\n");
-				IRCConnection.send("NICK " + getBotUsername() + "\r\n");
+				connection.send("PASS oauth:" + connection.getUserPassword() + "\r\n");
+				connection.send("NICK " + connection.getUsername() + "\r\n");
 				
-				if (getExtendedInformation()) {
-					IRCConnection.send("CAP REQ :twitch.tv/membership \r\n");
-					IRCConnection.send("CAP REQ :twitch.tv/commands \r\n");
-					IRCConnection.send("CAP REQ :twitch.tv/tags \r\n");
+				if (extendedInformation) {
+					connection.send("CAP REQ :twitch.tv/membership \r\n");
+					connection.send("CAP REQ :twitch.tv/commands \r\n");
+					connection.send("CAP REQ :twitch.tv/tags \r\n");
 				}
 			} else {
 				return 1;
@@ -119,28 +54,58 @@ public class TwitchIRCClient extends IRCClient{
 		return 0;
 	}
 
-	@Override
-	public int disconnect() {
+	public static int disconnect(TwitchIRCConnection connection) {
 		
-		if (IRCConnection.getConnectionStatus() == 1) {
-			IRCConnection.disconnect();
+		if (connection.getConnectionStatus() == 1) {
+			connection.disconnect();
 		}
 		
 		return 0;
 	}
 
-	@Override
-	public int restart() {
+	public static int restart(TwitchIRCConnection connection, boolean extendedInformation) {
 		
-		if (IRCConnection.getConnectionStatus() == 0 || IRCConnection.getConnectionStatus() == 2) {
-			connect();
+		if (connection.getConnectionStatus() == 0 || connection.getConnectionStatus() == 2) {
+			TwitchIRCClient.connect(connection, extendedInformation);
 		}
 		
-		if (IRCConnection.getConnectionStatus() == 1) {
-			disconnect();
-			connect();
+		if (connection.getConnectionStatus() == 1) {
+			TwitchIRCClient.disconnect(connection);
+			TwitchIRCClient.connect(connection, extendedInformation);
 		}
 		
 		return 0;
+	}
+	
+	public static int joinChannel(String channelName, TwitchIRCConnection connection) {
+		return TwitchIRCClient.sendServerMessage("JOIN #" + channelName, connection);
+	}
+	
+	public static int departChannel(String channelName, TwitchIRCConnection connection) {
+		return TwitchIRCClient.sendServerMessage("PART #" +channelName, connection);
+	}
+	
+	public static String readMessage(TwitchIRCConnection connection) {
+		String message = "";
+		
+		message = connection.receive();
+		
+		if (message.toLowerCase().startsWith("ping")) {
+			TwitchIRCClient.sendServerMessage("PONG :tmi.twitch.tv", connection);
+		}
+		
+		return message;
+	}
+	
+	public static int sendServerMessage(String message, TwitchIRCConnection connection) {
+		return connection.send(message + "\r\n");
+	}
+	
+	public static int sendChannelMessage(String channelName, String message, TwitchIRCConnection connection) {
+		return TwitchIRCClient.sendServerMessage("PRIVMSG #" + channelName + " :" + message, connection);
+	}
+	
+	public static int sendWisperMessage(String username, String message, TwitchIRCConnection connection) {
+		return TwitchIRCClient.sendServerMessage(".w " + username + " " + message, connection);
 	}
 }
